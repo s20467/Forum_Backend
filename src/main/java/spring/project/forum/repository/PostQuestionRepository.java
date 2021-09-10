428e6f06db41cea428724b6f829b01778aa527c4
Hubert Orzełowski <s20467@pja.edu.pl>
2021-09-10 15:14:51 +0200

    Fill PostQuestionRepository with needed methods

--- src/main/java/spring/project/forum/repository/PostQuestionRepository.java
+++ src/main/java/spring/project/forum/repository/PostQuestionRepository.java
@@ -1,7 +1,19 @@
 package spring.project.forum.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import spring.project.forum.model.PostAnswer;
 import spring.project.forum.model.PostQuestion;
+import spring.project.forum.model.security.User;
+
+
+import java.util.List;
 
 public interface PostQuestionRepository extends JpaRepository<PostQuestion, Integer> {
+    Page<PostQuestion> findAllByAuthor(Pageable pageable, User user);
+    List<PostQuestion> findAllByAuthor(User user);
+    List<PostQuestion> findAllByBestAnswerIsNull();
+    Page<PostQuestion> findAllByBestAnswerIsNull(Pageable pageable);
+
 }
