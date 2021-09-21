c13f4e8a47bdab34722b59304417b1cfe564b207
Hubert Orzełowski <s20467@pja.edu.pl>
2021-09-21 15:34:19 +0200

    Create necessary security beans

---
+++ src/main/java/spring/project/forum/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package spring.project.forum.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import spring.project.forum.repository.security.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+    }
+}
--- src/main/java/spring/project/forum/security/SecurityBeans.java
+++ src/main/java/spring/project/forum/security/SecurityBeans.java
@@ -1,2 +1,15 @@
-package spring.project.forum.security;public class SecurityBeans {
+package spring.project.forum.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
