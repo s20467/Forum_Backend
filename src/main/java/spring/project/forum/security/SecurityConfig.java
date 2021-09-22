8a13eaa4a4bd1a0f127885d7f0818f05f21d3d6a
Hubert Orzełowski <s20467@pja.edu.pl>
2021-09-22 17:19:47 +0200

    Enable method level security

--- src/main/java/spring/project/forum/security/SecurityConfig.java
+++ src/main/java/spring/project/forum/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package spring.project.forum.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
