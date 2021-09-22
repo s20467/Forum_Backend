a429f9c3f916795df278c23c03d04502239ffd86
Hubert Orzełowski <s20467@pja.edu.pl>
2021-09-22 17:24:49 +0200

    Implement security parts in controller

--- src/main/java/spring/project/forum/api/v1/controller/PostController.java
+++ src/main/java/spring/project/forum/api/v1/controller/PostController.java
@@ -2,6 +2,7 @@ package spring.project.forum.api.v1.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import spring.project.forum.api.v1.dto.AnswerDto;
 import spring.project.forum.api.v1.dto.QuestionDto;
@@ -9,7 +10,6 @@ import spring.project.forum.model.Answer;
 import spring.project.forum.model.Question;
 import spring.project.forum.service.AnswerService;
 import spring.project.forum.service.QuestionService;
-
 import java.util.List;
 
 @RestController
@@ -19,21 +19,25 @@ public class PostController {
     private final QuestionService questionService;
     private final AnswerService answerService;
 
+
     public PostController(QuestionService questionService, AnswerService answerService) {
         this.questionService = questionService;
         this.answerService = answerService;
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("questions/{questionId}")
-    public Question getQuestionById(@PathVariable("questionId") Integer id){
-        return questionService.getById(id);
+    public Question getQuestionById(@PathVariable("questionId") Integer questionId){
+        return questionService.getById(questionId);
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("questions")
     public List<Question> getQuestions(){
         return questionService.getAll();
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping(value = "questions", params = {"page", "limit", "sort"})
     public Page<Question> getQuestions(
             @RequestParam("page") Integer pageNum,
@@ -42,17 +46,20 @@ public class PostController {
         return questionService.getAll(pageNum, pageSize, sortBy);
     }
 
+    @PreAuthorize("hasAuthority('user.question.create')")
     @PostMapping("questions")
     @ResponseStatus(HttpStatus.CREATED)
     public Question createQuestion(@RequestBody QuestionDto questionDto){
         return questionService.createQuestion(questionDto);
     }
 
+    @PreAuthorize("hasAuthority('admin.question.close') or (isFullyAuthenticated() and @customAuthenticationManager.isQuestionOwner(authentication, #questionId))")
     @GetMapping("questions/{questionId}/close")
     public Question closeQuestion(@PathVariable("questionId") Integer questionId){
         return questionService.closeQuestion(questionId);
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping(value = "questions/{questionId}/answers", params = {"page", "limit", "sort"})
     public Page<Answer> getAnswersForQuestion(
             @PathVariable("questionId") Integer questionId,
@@ -62,65 +69,73 @@ public class PostController {
         return answerService.getByQuestion(questionId, pageNum, pageSize, sortBy);
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("questions/{questionId}/answers")
     public List<Answer> getAnswersForQuestion(@PathVariable("questionId") Integer questionId){
         return answerService.getByQuestion(questionId);
     }
 
+    @PreAuthorize("isFullyAuthenticated()")
     @PostMapping("questions/{questionId}/give-answer")
     public Answer giveAnswer(@PathVariable("questionId") Integer questionId, @RequestBody AnswerDto answerDto){
         return answerService.createAnswerForQuestion(questionId, answerDto);
     }
 
+    @PreAuthorize("isFullyAuthenticated()")
     @GetMapping("questions/{questionId}/upvote")
     public Question upVoteQuestion(@PathVariable("questionId") Integer questionId){
-        //todo implement upVoteQuestion in PostController
-        return null;
+        return questionService.upVote(questionId);
     }
 
+    @PreAuthorize("isFullyAuthenticated()")
     @GetMapping("questions/{questionId}/downvote")
     public Question downVoteQuestion(@PathVariable("questionId") Integer questionId){
-        //todo implement downVoteQuestion in PostController
-        return null;
+        return questionService.downVote(questionId);
     }
 
+    @PreAuthorize("isFullyAuthenticated()")
     @GetMapping("answers/{answerId}/upvote")
     public Answer upVoteAnswer(@PathVariable("answerId") Integer answerId){
-        //todo implement upVoteAnswer in PostController
-        return null;
+        return answerService.upVote(answerId);
     }
 
+    @PreAuthorize("isFullyAuthenticated()")
     @GetMapping("answers/{answerId}/downvote")
     public Answer downVoteAnswer(@PathVariable("answerId") Integer answerId){
-        //todo implement downVoteAnswer in PostController
-        return null;
+        return answerService.downVote(answerId);
     }
 
+    @PreAuthorize("hasAuthority('admin.question.set-unset-best-answer') or (isFullyAuthenticated() and @customAuthenticationManager.isQuestionOwner(authentication, #questionId))")
     @GetMapping("questions/{questionId}/set-best-answer/{answerId}")
     public Question setBestAnswer(@PathVariable("questionId") Integer questionId, @PathVariable("answerId") Integer answerId){
         return questionService.setBestAnswer(questionId, answerId);
     }
 
+    @PreAuthorize("hasAuthority('admin.question.set-unset-best-answer') or (isFullyAuthenticated() and @customAuthenticationManager.isQuestionOwner(authentication, #questionId))")
     @GetMapping("questions/{questionId}/unset-best-answer")
     public Question unsetBestAnswer(@PathVariable("questionId") Integer questionId){
         return questionService.unsetBestAnswer(questionId);
     }
 
+    @PreAuthorize("hasAuthority('admin.question.delete') or (isFullyAuthenticated() and @customAuthenticationManager.isQuestionOwner(authentication, #questionId))")
     @DeleteMapping("questions/{questionId}")
     public void deleteQuestion(@PathVariable("questionId") Integer questionId){
         questionService.deleteById(questionId);
     }
 
+    @PreAuthorize("hasAuthority('admin.answer.delete') or (isFullyAuthenticated() and @customAuthenticationManager.isAnswerOwner(authentication, #answerId))")
     @DeleteMapping("answers/{answerId}")
     public void deleteAnswer(@PathVariable("answerId") Integer answerId){
        answerService.deleteById(answerId);
     }
 
+    @PreAuthorize("hasAuthority('admin.question.update') or (isFullyAuthenticated() and @customAuthenticationManager.isQuestionOwner(authentication, #questionId))")
     @PatchMapping("questions/{questionId}")
     public Question updateQuestion(@PathVariable("questionId") Integer questionId, @RequestBody QuestionDto questionDto){
         return questionService.updateQuestion(questionId, questionDto);
     }
 
+    @PreAuthorize("hasAuthority('admin.answer.update') or (isFullyAuthenticated() and @customAuthenticationManager.isAnswerOwner(authentication, #answerId))")
     @PatchMapping("answers/{answerId}")
     public Answer updateAnswer(@PathVariable("answerId") Integer answerId, @RequestBody AnswerDto answerDto){
         return answerService.updateAnswerContent(answerId, answerDto);
