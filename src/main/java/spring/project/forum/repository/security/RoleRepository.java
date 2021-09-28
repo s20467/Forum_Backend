7574ecbb45506b46764c71d6caeacfea2d0a17a0
Hubert Orzełowski <s20467@pja.edu.pl>
2021-09-28 13:33:46 +0200

    Remove unnecessary classes

--- src/main/java/spring/project/forum/repository/security/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.project.forum.repository.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.project.forum.model.security.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
